c380840aefac8be02e152e68a6a2a0846e164afb
Jake Zien <jake@floored.com>
2019-04-09 00:18:54 -0400

    zoetrope

---
+++ src/components/Zoetrope.js
@@ -0,0 +1,12 @@
+import React from "react"
+import styles from "./Zoetrope.module.styl"
+
+
+const Zoetrope = () => (
+  <div className={styles.container}>
+    <div className={styles.wheel}></div>
+    {/*<div className={styles.cover}>A&J</div>*/}
+  </div>
+)
+
+export default Zoetrope
--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,14 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import Image from "../components/image"
-import Container from "../components/Container"
+import Container from "./Container"
 import styles from "./header.module.styl"
+import Zoetrope from "./Zoetrope"
 
 
 const Header = ( props ) => (
   <header id={styles.siteHeader}>
+    <Zoetrope/>
     <Container>
       <h1>{props.siteTitle}</h1>
     </Container> 
