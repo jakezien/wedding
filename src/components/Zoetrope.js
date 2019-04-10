79670d3cc41ba1a9053fa80bdaf881c0005739e7
Jake Zien <jake@floored.com>
2019-04-09 22:32:35 -0400

    better zoetrope

--- src/components/Zoetrope.js
+++ src/components/Zoetrope.js
@@ -1,10 +1,14 @@
 import React from "react"
 import styles from "./Zoetrope.module.styl"
+import image from '../images/zoetrope.png'
 
 
 const Zoetrope = () => (
   <div className={styles.container}>
-    <div className={styles.wheel}></div>
+    <div className={styles.imgWrap}>
+      <img src={image} alt="zoetrope"/>
+    </div>
+    {/*<div className={styles.wheel}></div>*/}
     {/*<div className={styles.cover}>A&J</div>*/}
   </div>
 )
