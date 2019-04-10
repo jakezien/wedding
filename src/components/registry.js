818bb329e63ca8df8ff8d45dee15447469cbf862
Jake Zien <jake@floored.com>
2019-04-09 23:13:21 -0400

    added pomegranate

--- src/components/registry.js
+++ src/components/registry.js
@@ -2,10 +2,13 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import Image from "../components/image"
+import img from "../images/pomegranate.png"
+
 
 const Registry = ({urls}) => (
   <section id="registry">
     <h2>Registry</h2>
+    <img class="interstitial" src={img}/>
     <div class="left">
       <p>Your presence is plenty — but if you insist, we’ve registered via <a href={urls.zola} target="_blank">Zola.</a></p>
       <p>Our shipping address, if you need it, is 80 Douglass Street, Brooklyn, New York, 11231.</p>
