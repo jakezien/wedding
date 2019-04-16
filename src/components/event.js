55831a839d7da5f55f3ca7c61cef7ce663c490f9
Jake Zien <jake@floored.com>
2019-04-16 00:24:35 -0400

    adults only

--- src/components/event.js
+++ src/components/event.js
@@ -7,7 +7,8 @@ const Event = () => (
     <img className="interstitial" src={img} alt="Illustration of the number 19 surrounded by snakes"/>
     <h2>The Event</h2>
     <div>
-      <p>The ceremony will be at The Prospect Park Boathouse, at 5:30 PM on October 19, 2019. Drinks, dinner and dancing will follow.</p>
+      <p>The ceremony will be at The Prospect Park Boathouse, at 5:30 PM on October 19, 2019. Drinks, dinner and dancing will follow.</p> 
+      <p>Kindly note that this is an adults-only evening.</p>
       <p>The dress code is formal. Two notes about what to wear:</p>
         <ul>
           <li>Please choose your heels carefully, as the boathouse has cobbled floors.</li>
