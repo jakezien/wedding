6ef795a67cbe14ca07a038bfe23143190af511f6
Jake Zien <jake@floored.com>
2019-04-15 23:31:15 -0400

    text update

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,7 +5,7 @@ import Container from "../components/Container"
 const Intro = () => (
   <section id="intro">
     <Container>
-      <p className="center">We&rsquo;re so excited you&rsquo;re here! We can&rsquo;t wait to celebrate with you as we start our next phase of life together.</p>
+      <p className="center">We&rsquo;re so excited you&rsquo;re here! We can&rsquo;t wait to celebrate with you as we start the next phase of our life together.</p>
     </Container>
   </section>
 )
