3738b247d227a2c91e98bd7c22de8052381adb0b
Jake Zien <jake@floored.com>
2019-04-16 16:48:02 -0400

    Lucy edits and italic font

--- src/components/event.js
+++ src/components/event.js
@@ -7,7 +7,7 @@ const Event = () => (
     <img className="interstitial" src={img} alt="Illustration of the number 19 surrounded by snakes"/>
     <h2>The Event</h2>
     <div>
-      <p>The ceremony will be at The Prospect Park Boathouse, at 5:30 PM on October 19, 2019. Drinks, dinner and dancing will follow.</p> 
+      <p>The ceremony will be at The Prospect Park Boathouse, at 5:30 p.m. on October 19, 2019. Drinks, dinner and dancing will follow.</p> 
       <p>Kindly note that this is an adults-only evening.</p>
       <p>The dress code is formal. Two notes about what to wear:</p>
         <ul>
