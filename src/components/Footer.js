c00705212959db3cdda247930d837bd7f971591c
Jake Zien <jake@floored.com>
2019-10-11 13:00:44 -0400

    updated countdown

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,13 +5,22 @@ import Countdown from 'react-countdown-now'
 
 
 const renderCountdown = ({days}) => {
-  return <span className={styles.countdown}>{days}</span>
+  let content = ''
+
+  if (days > 1) content = days + ' days!'
+  else if (days === 1) content = 'Tomorrow!'
+  else if (days === 0) content = 'Today’s the day!'
+  else if (days < 0) content = ''
+  
+  return <span className={styles.countdown}>{content}</span>
 }
 
 const Footer = () => (
   <footer id={styles.siteFooter}>
     <Container>
-      <h2 className={styles.footerText}><Countdown date="2019-10-20" renderer={renderCountdown} /> days!</h2>
+      <h2 className={styles.footerText}>
+        <Countdown date="2019-10-20" renderer={renderCountdown} />
+      </h2>
     </Container>
   </footer>
 )
